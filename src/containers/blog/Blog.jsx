2eab1c15ddf146c75f3e5481d4837924908bfd62
Saúl Camacho <electrohead1200@gmail.com>
2024-05-29 18:12:19 -0600

    Blog & Article details

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -20,25 +20,21 @@ const Blog = () => {
         </div>
         <div className="gpt3__blog-container_groupB">
           <Article
-            className="gpt3__blog-container_article"
             imgUrl={blog02}
             date="Sep 26, 2021"
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
           />
           <Article
-            className="gpt3__blog-container_article"
             imgUrl={blog03}
             date="Sep 26, 2021"
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
           />
           <Article
-            className="gpt3__blog-container_article"
             imgUrl={blog04}
             date="Sep 26, 2021"
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
           />
           <Article
-            className="gpt3__blog-container_article"
             imgUrl={blog05}
             date="Sep 26, 2021"
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
